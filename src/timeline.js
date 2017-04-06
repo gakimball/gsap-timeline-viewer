da3f40ae908e6b50116a3109eb1c72c92de58735
Geoff Kimball <geoff@geoffkimball.com>
2017-04-06 16:13:08 -0700

    Organize timeline nodes into rows based on free space

--- src/timeline.js
+++ src/timeline.js
@@ -6,10 +6,62 @@ export default class Timeline extends Component {
     timeline: PropTypes.instanceOf(TimelineLite).isRequired
   }
 
+  static organizeTimeline(timeline) {
+    const items = timeline.getChildren(false);
+    const rows = [];
+    const freeRow = item => {
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        let freeSpace = true;
+
+        for (let i = 0; i < row.items.length; i++) {
+          const segment = row.items[i];
+          const itemStart = item.startTime();
+          const itemEnd = item.endTime();
+          const segmentStart = segment.startTime();
+          const segmentEnd = segment.endTime();
+
+          if (itemStart >= segmentStart && itemStart <= segmentEnd) {
+            freeSpace = false;
+          } else if (itemEnd >= segmentStart && itemEnd <= segmentEnd) {
+            freeSpace = false;
+          }
+        }
+
+        if (freeSpace) {
+          return i;
+        }
+      }
+
+      return -1;
+    };
+    const addRow = segment => rows.push({
+      line: rows.length,
+      items: [segment]
+    });
+
+    for (const item of items) {
+      if (rows.length === 0) {
+        addRow(item);
+      } else {
+        const index = freeRow(item);
+        if (index > -1) {
+          rows[index].items.push(item);
+        } else {
+          addRow(item);
+        }
+      }
+    }
+
+    return rows;
+  }
+
   constructor(props) {
     super(props);
 
-    // console.log(props.timeline.getChildren(false));
+    this.state = {
+      rows: this.constructor.organizeTimeline(props.timeline)
+    };
   }
 
   getPercentage(time) {
@@ -20,56 +72,57 @@ export default class Timeline extends Component {
 
   render() {
     const {timeline} = this.props;
+    const {rows} = this.state;
 
     return (
       <div className="timeline">
-        {timeline.getChildren(false).map((item, index) => {
-          const style = {
-            left: this.getPercentage(item.startTime()),
-            right: this.getPercentage(timeline.totalDuration() - item.endTime())
-          };
+        {rows.map(row =>
+          <div className="row" key={row.line}>
+            {row.items.map((item, index) => {
+              const style = {
+                left: this.getPercentage(item.startTime()),
+                right: this.getPercentage(timeline.totalDuration() - item.endTime())
+              };
 
-          if (item instanceof TimelineLite) {
-            // Render timelines
-            return (
-              <div key={index} className="node node--timeline">
-                <div className="node__inner" style={style}>
-                  Timeline
+              if (item instanceof TimelineLite) {
+                // Render timelines
+                return (
+                  <div className="node node--timeline" style={style} key={index} >
+                    Timeline
+                  </div>
+                );
+              }
+
+              // Render tweens
+              return (
+                <div className="node node--tween" style={style} key={index}>
+                  {item._propLookup.reduce((arr, item) =>
+                    arr.concat(Object.keys(item))
+                  , []).join(', ')}
                 </div>
-              </div>
-            );
-          }
-
-          // Render tweens
-          return (
-            <div key={index} className="node node--tween">
-              <div className="node__inner" style={style}>
-                {item._propLookup.reduce((arr, item) =>
-                  arr.concat(Object.keys(item))
-                , []).join(', ')}
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </div>
+        )}
 
         <style jsx>{`
-          .node {
+          .row {
             position: relative;
             width: 100%;
             height: 20px;
           }
 
-          .node__inner {
+          .node {
             position: absolute;
             top: 0;
             bottom: 0;
           }
 
-          .node--tween .node__inner {
+          .node--tween {
             background: #afeef0
           }
 
-          .node--timeline .node__inner {
+          .node--timeline {
             background: #d5a6b7
           }
         `}</style>
