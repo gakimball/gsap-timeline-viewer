0479a4a8b26c1022228a7ef9d6e6c8ab0bf96d08
Geoff Kimball <geoff@geoffkimball.com>
2017-04-07 12:29:40 -0700

    Refactor <Node /> to have less branching code

--- src/node.js
+++ src/node.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {TimelineLite} from 'gsap';
+import cls from 'classnames';
 
 const getPercentage = (time, total) => {
   const start = parseFloat(time);
@@ -7,83 +8,92 @@ const getPercentage = (time, total) => {
   return `${(start / end).toFixed(2) * 100}%`;
 };
 
-const css = (
-  <style jsx>{`
-    .node {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      font-size: 11px;
-      line-height: 20px;
-      text-align: center;
-      text-transform: uppercase;
-      font-weight: bold;
-      color: rgba(0, 0, 0, 0.5);
-    }
-
-    .node--instant {
-      right: auto !important;
-      width: 20px;
-      height: 20px;
-      margin-left: -10px;
-      border-radius: 50%;
-      text-align: center;
-    }
-
-    .node--tween {
-      background: #afeef0
-    }
-
-    .node--timeline {
-      background: #d5a6b7
-    }
-
-    .node--function {
-      background: #a7f1ba;
-    }
-
-    .node--set {
-      background: #97ddd6;
-    }
-  `}</style>
-);
-
 const Node = ({item, timeline}) => {
   const style = {
     left: getPercentage(item.startTime(), timeline.totalDuration()),
     right: getPercentage(timeline.totalDuration() - item.endTime(), timeline.totalDuration())
   };
+  const isInstant = item.startTime() === item.endTime();
+  const isFunction = typeof item.target === 'function';
+  const nodeType = do {
+    if (item instanceof TimelineLite) {
+      'timeline';
+    } else if (isInstant) {
+      if (isFunction) {
+        'function';
+      } else {
+        'set';
+      }
+    } else {
+      'tween';
+    }
+  };
 
-  // Render timelines
-  if (item instanceof TimelineLite) {
-    return (
-      <div className="node node--timeline" style={style}>
-        {/* <Timeline timeline={item}/> */}
-        Timeline
-        {css}
-      </div>
-    );
-  }
+  const className = cls('node', {
+    'node--instant': isInstant,
+    'node--timeline': nodeType === 'timeline',
+    'node--set': nodeType === 'set',
+    'node--function': nodeType === 'function',
+    'node--tween': nodeType === 'tween'
+  });
 
-  // Render items with no duration (callbacks, sets)
-  if (item.startTime() === item.endTime()) {
-    const type = typeof item.target === 'function' ? 'function' : 'set';
-    return (
-      <div className={`node node--instant node--${type}`} style={style}>
-        {type === 'function' ? 'F' : 'S'}
-        {css}
-      </div>
-    );
-  }
-
-  // Render tweens
-  const properties = Array.isArray(item._propLookup) ? item._propLookup : [];
   return (
-    <div className="node node--tween" style={style}>
-      {properties.reduce((arr, item) =>
-        arr.concat(Object.keys(item))
-      , []).join(', ')}
-      {css}
+    <div className={className} style={style}>
+      {(() => {
+        switch (nodeType) {
+          case 'tween': {
+            const properties = Array.isArray(item._propLookup) ? item._propLookup : [];
+            return properties.reduce((arr, item) =>
+              arr.concat(Object.keys(item))
+            , []).join(', ');
+          }
+          case 'timeline':
+            return 'Timeline';
+          case 'function':
+            return 'F';
+          case 'set':
+            return 'S';
+          default:
+        }
+      })()}
+      <style jsx>{`
+        .node {
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          font-size: 11px;
+          line-height: 20px;
+          text-align: center;
+          text-transform: uppercase;
+          font-weight: bold;
+          color: rgba(0, 0, 0, 0.5);
+        }
+
+        .node--instant {
+          right: auto !important;
+          width: 20px;
+          height: 20px;
+          margin-left: -10px;
+          border-radius: 50%;
+          text-align: center;
+        }
+
+        .node--tween {
+          background: #afeef0
+        }
+
+        .node--timeline {
+          background: #d5a6b7
+        }
+
+        .node--function {
+          background: #a7f1ba;
+        }
+
+        .node--set {
+          background: #97ddd6;
+        }
+      `}</style>
     </div>
   );
 };
