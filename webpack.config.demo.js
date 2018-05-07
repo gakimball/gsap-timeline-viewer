c487e6b065c13399484aedd184630703ded1f10d
Geoff Kimball <geoff@geoffkimball.com>
2018-05-07 11:08:24 -0700

    Add production build

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,4 +1,17 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const plugins = [];
+
+if (PRODUCTION) {
+  plugins.push(new UglifyJsPlugin({
+    uglifyOptions: {
+      mangle: false,
+    },
+  }))
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'demo'),
@@ -17,5 +30,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins,
 };
